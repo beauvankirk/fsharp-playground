cc4537fa5199153a4f502935dbde91d18d55e1c9
Beau Van Kirk <beauvankirk@gmail.com>
2020-06-28 23:48:22 -0500

    first commit

---
+++ src/classlib-template/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace classlib_template
+open FSharp.ComProvider
+module Say =
+
+    let hello name =
+        printfn "Hello %s" name
+
+    let useComProvider name =
+        printfn "Hello %s" name
---
+++ src/websharper-lib/Library.fs
@@ -0,0 +1,7 @@
+﻿namespace websharper_lib
+
+open WebSharper
+
+[<JavaScript>]
+type Class1() = 
+    member this.X = "F#"
